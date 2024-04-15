de826cfb6081450fc181cda13c60e7ceee57fabe
Patrick Kelly <patdkelly5@gmail.com>
2024-04-16 09:07:35 +1000

    flat() and flatmap() method notes

--- 11 Arrays/script.js
+++ 11 Arrays/script.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Patrick Kelly',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Lauren Marshall',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -441,11 +471,40 @@ console.log(movements.some(deposit));
 // flat and flatMap Methods
 //========================================
 //
+// flat()
+//========================================
 // I can use the flat method to basically COMBINE all the elements of a 2D array, and join them together into ONE array
 // So flat removes the nested arrays, and flattens the array overall
 const lozArr = [[1, 2, 3], [4, 5, 6], 7, 8];
 console.log(lozArr.flat());
 
 // But the flat method only goes ONE LEVEL DEEP when flattening
+// But with flat I can actually specify how many levels of nesting deep I want to flatten
 const lozArrDeep = [[[1, 2, 3], [4, 5, 6], 7, 8]];
-console.log(lozArrDeep.flat());
+console.log(lozArrDeep.flat(2));
+
+// So let's say the bank wants to count up ALL the movements across ALL bank accounts:
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+// const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+
+// But I can shorten all that horseshit down to something much more clean and more pleasing to look at:
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance); // 17840
+//
+// flatMap()
+//========================================
+//
+// flatmap essentially combines both the flat method AND the map method
+// The only issue with using flatMap() is that IT ONLY GOES ONE LEVEL DEEP
+// So it's a good use-case for single-level nesting, but not multiple
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance); // 17840
