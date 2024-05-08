449113450c4e6dbc3f34f0431b181748dd3f0eb5
Patrick Kelly <patdkelly5@gmail.com>
2024-05-08 22:58:32 +1000

    Finished refactoring ccoe for modularity, added more date/time functionality

--- 12 Numbers, Dates, Times/starter/script.js
+++ 12 Numbers, Dates, Times/starter/script.js
@@ -81,6 +81,30 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    // 1000ms in 1 second
+    // 60secs in 1 minute
+    // 60mins in 1 hour
+    // 24hrss in 1 day
+    Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0); // zero-based
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+
+  // If I need really advanced and/or precise time differentials, like AEST/AEDT, then I should use a FREE date library, like Moment.JS
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -92,10 +116,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0); // zero-based
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
@@ -557,14 +578,3 @@ console.log(new Date(account1.movementsDates[0]));
 
 const future = new Date(2037, 10, 19, 15, 23);
 console.log(+future);
-const calcDaysPassed = (date1, date2) =>
-  // 1000ms in 1 second
-  // 60secs in 1 minute
-  // 60mins in 1 hour
-  // 24hrss in 1 day
-  Math.abs((date2 - date1) / (1000 * 60 * 60 * 24));
-
-const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
-console.log(days1); // 864000000ms
-
-// If I need really advanced and/or precise time differentials, like AEST/AEDT, then I should use a FREE date library, like Moment.JS
