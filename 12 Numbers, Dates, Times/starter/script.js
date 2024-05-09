babce020a9b0f64a7a7e1c9afdb7576fa2a9c804
Patrick Kelly <patdkelly5@gmail.com>
2024-05-09 15:30:55 +1000

    Fixed the sort button bug: was passing only the movements array instead of the entire accounts object containing the array(s)

--- 12 Numbers, Dates, Times/starter/script.js
+++ 12 Numbers, Dates, Times/starter/script.js
@@ -359,7 +359,9 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  // Original bug that broke the sort button:
+  // displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
