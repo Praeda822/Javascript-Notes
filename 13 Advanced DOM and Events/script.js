58418245236c02adf98c2e508eb574e82c9ed412
Patrick Kelly <patdkelly5@gmail.com>
2024-05-10 18:22:55 +1000

    Theory notes on DOM manipulation

--- 13 Advanced DOM and Events/script.js
+++ 13 Advanced DOM and Events/script.js
@@ -29,3 +29,54 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+//
+//
+//========================================
+// Selecting Elements
+//========================================
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('#section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+//
+//
+//========================================
+// Creating and inserting elements
+//========================================
+//
+// A quick and easy way to add elements to the DOM:
+// .insertAdjacentHTML
+
+const msg = document.createElement('div');
+msg.classList.add('cookie-msg');
+// msg.textContent = 'We use cookies for improved functionality and analytics';
+msg.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// The msg in my header is a live element in the DOM, so it can only appear once
+// But I can still use prepend/append to move the element around the DOM
+// header.prepend(msg);
+header.append(msg);
+// If I want to add multiple elements to the DOM:
+// header.append(msg.cloneNode(true));
+// Here it's BEFORE my header, right at the top:
+header.before(msg);
+// And here it's right at the END of the header:
+// header.after(msg);
+//
+//========================================
+// Deleting elements
+//========================================
+//
+// Here I can use basic DOM manipulation to delete elements:
+// By clicking the button, with my event listener attached, the entire element is dynamically removed
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    msg.remove();
+  });
