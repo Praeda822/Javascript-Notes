075c0b235cdd7689e5d96bf9e371963f3166b082
Patrick Kelly <patdkelly5@gmail.com>
2024-05-10 18:46:25 +1000

    Upload of styles, attributes, and classes theory and prac

--- 13 Advanced DOM and Events/script.js
+++ 13 Advanced DOM and Events/script.js
@@ -80,3 +80,74 @@ document
   .addEventListener('click', function () {
     msg.remove();
   });
+//
+//
+//========================================
+// Styles, Attributes, and Classes
+//========================================
+//
+//
+// Styles
+//========================================
+//
+// Styles that are set directly in the DOM are INLINE
+msg.style.backgroundColor = '#37383d';
+msg.style.width = '120%';
+// Using inline styles and DOM manipulation I can rip the colors from a website using the console
+// This doesn't work for colors nested within CSS classes, only the inline styles I define
+console.log(msg.style.backgroundColor);
+// I can still however get the style definitions using getComputedStyle and defining what style I want to
+console.log(getComputedStyle(msg).color);
+// I need to convert the string back into a number using Number.parseFloat()
+msg.style.height =
+  Number.parseFloat(getComputedStyle(msg).height, 10) + 30 + 'px';
+// I can easily change the stytle of my page by setting a property: the name, and the value
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+//
+//
+// Attributes
+//========================================
+//
+// Standard:
+// I can also use DOM manipulation to extract the values of element attributes:
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt); // Bankist Logo
+
+console.log(logo.className); // nav__logo
+//
+// Non-Standard:
+
+// Just like how I can READ attributes
+// I can also set them:
+logo.alt = 'beautiful minimalist logo';
+logo.setAttribute('company', 'bankist');
+
+console.log(logo.src); // localhost/img/logo.png
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+//
+//
+// Data Attributes
+//========================================
+//
+// I'm going to tend to use data ttributes a lot when working with UI/UX design, especially when I need to STORE DATA in the user interface
+// I can also get the special attribute of dataSet:
+console.log(logo.dataset.versionNumber);
+//
+//
+// Classes
+//========================================
+//
+// I really only need to know the following four methods for mucking around with my classes:
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // NOT INCLUDES
+
+// I SHOULD NOT use this one as it will override any classes I have on the element and override them with whatever I've specified
+// That's why the bankist logo looks like hot garbage now
+logo.className = 'Patrick';
