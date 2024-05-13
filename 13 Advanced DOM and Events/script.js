bd3dae057b5d4871072ffbe5be5f1c234313f259
Patrick Kelly <patdkelly5@gmail.com>
2024-05-13 16:57:21 +1000

    THIRD iteration of the sticky nav functionality...

--- 13 Advanced DOM and Events/script.js
+++ 13 Advanced DOM and Events/script.js
@@ -449,3 +449,20 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 // const observer = new IntersectionObserver(obsCallBack, opts);
 // observer.observe(section1);
+
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  rootMargin: `-${navHeight}px`,
+  threshold: 0,
+});
+headerObserver.observe(header);
