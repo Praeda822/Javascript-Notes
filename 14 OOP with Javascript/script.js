d8da5b96012705e8daf7e59df7482a205e2000fc
Patrick Kelly <patdkelly5@gmail.com>
2024-05-17 19:27:58 +1000

    Upload of MORE Data Science > OOP in Javascript. SECTION 9 STUFF!!!

--- 14 OOP with Javascript/script.js
+++ 14 OOP with Javascript/script.js
@@ -48,3 +48,69 @@
 // In OOP, Polymorphism however is the process in which a child class can overwrite a method it has inherited from its respective parent class
 // Going back to the three aforementioned classes, what if the Admin and Author users have their own special way of logging in?
 // Well, by utilising/understanding Polymorphism, I can enforce the rule that these respective classes will have their own working logins, despite them having the same logic, as the child classes login overwrites its parents
+//
+//
+// ========================================
+// OOP in Javascript
+// ========================================
+//
+// As I know, a "class" is basically like site plans but for code
+// In Javascript, classes can be used to create actual objects (object generation, remember?)called INSTANCES
+// This process, where Objects are created (generated) from a class, is called INSTANTIATION
+// And, as I also know, all objects in Javascript are: PROTOTYPES.
+// Remember the Data Science bullshit bonanza from Section 9 that you wanted to headbutt a wall doing after 3 rounds of it?
+// What's an Array (REALLY)? Well, it's technically speaking, an object that I am amble to pass methods() to
+// Remember, PASS the methods to, meaning it's the PROTOTYPE (Blueprint brooo) that ACTUALLY contains the methods, and the Objects themselves are BUILT FROM the site plans!! (what a fuck around trifecta)
+// Technically speaking, this is called
+
+// PROTYPAL INHERITANCE:
+// ========================================
+//
+// The prototype contains the methods (behaviours) that are ACCESSIBLE to all objects linked to that prototype
+// Remember though, man, this is the INVERSE version of how it's all MEANT to work with classes in Javascript: where classes instantiate (generate) the instances, and the parent>child classes being a CLASS->CLASS inheritance, respectively
+// Prototypal Inheritance is where an instance inherits from the class, which is again, inverse
+// The process of the behaviour of the methods linked to a prototype object is known as DELEGATION
+//
+// In classic OOP, the behaviour are actually copied from the classes to the objects themselves respectively
+//
+// If I look up arrays on the mdn, it's always formatted as Array.prototype.method()
+// For example, again using ARRAYS because who doesnt love arrays:
+const myNum = [1, 2, 3];
+myNum.map(v => v * 2);
+console.log(myNum);
+// So since Array.prototype is the original site plans (PROTOTYPE) of ALL array objects I create in Javascript, ALL arrays have access to the map method, where they are originally defined
+// So now myNum is linked to the prototype, and it inherits, or iS DELEGATED, all of the array methods
+// Finally, reiterating the importance that the map method is NOT defined on the numArray itself, but built from its prototype
+//
+//
+// ========================================
+// Implementing OOP in Javascript
+// ========================================
+//
+// So with that headache behind us, how can I actually create prototypes then?
+// How do I even link my objects TO my prototypes in the first place?
+// How can I even create new objects without having any classes to define them, or instantiate them, in the first place???? WTF
+// That's easy, my man, we can do it using:
+//
+// 1. Constructor functions
+// ========================================
+//
+// This is a programming tecnique where I can create objects from a function!
+// This is also how built-in objects like Arrays, Maps, and/or Sets are actually implemented in Javascript
+//
+//
+// 2. ES6 Classes
+// ========================================
+//
+// These are the modern alternative to using the constructor function syntax
+// It's, in all truth, really "Syntactic Sugar"
+// Aka, it works, but it's unnecessarily showy
+// yes, good for you, your dick is huge,and then everybody clapped, etc.
+// In reality, though, we all know that ES6 classes don't behave like classes in traditional OOP: they're subject to the prototypal inheritance system
+// Where it's a prototype that contains all the methods, and that then delegates those methods out to the LINKED objects
+//
+//
+// 3. Object.create()
+// ========================================
+//
+// This is the easiest and most straightforward way of linking an object to a prototype object
