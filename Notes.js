c56a23c8d0fee5e1356c355fff2a53e6d3d26065
Patrick Kelly <patdkelly5@gmail.com>
2024-05-25 23:05:33 +1000

    FIrst commit of upates theory notes

---
+++ Notes.js
@@ -0,0 +1,40 @@
+//========================================
+// **Computer Science Notes**
+//========================================
+
+// **Compilation vs Interpretation**
+//========================================
+
+// **Compilation** is a process where entire code is converted into machine code at once.
+// Then the code is written to a binary file that can be executed by a computer in the CPU.
+
+// **Interpretation** is a process where an *Interpreter* runs through the source code and **executes the code line by line**.
+
+// **Just-in-time Compilation** is a process that blends both **Compilation *and* Interpretation**
+// We achieve this by **compiling the entire code into machine code at once, then executing it right away**.
+
+//========================================
+// **What *is* Javascript?**
+//========================================
+
+// **High-level**
+//========================================
+
+// **Garbage Collection**
+//========================================
+// Javascript has in-built "Garbage Collection" which is an algorithm inside the Javascript engine that automatically old, unused objects from the computer's memory
+
+// **Interpreted or Just-in-time compiled**
+//========================================
+// Javascript is an interpreted or "just-in-time compiled" language
+
+// **Multi-paradigm**
+//========================================
+// Javascript is a multi-paradigm language
+// In programming, a **Paradigm** is an approach and mindset of structuring code, which will direct your coding style and technique
+// Three different programming paradigms are:
+//      1. Proecedural programming
+//      2. Object-oriented programming (OOP)
+//      3. Functional programming (FP)
+// Procedural programming is what I've been doing so far, just organising the code in a very linear way with some functions in between
+// We can also define many paradigms as **Imperative** vs. **Declarative**
