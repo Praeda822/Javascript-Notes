3b3159af0530dbb4367722204895e66d2b5511b1
Patrick Kelly <patdkelly5@gmail.com>
2024-05-27 17:01:11 +1000

    Pt. 1 of prototypal inheritance challenge #3

--- 14 OOP with Javascript/script.js
+++ 14 OOP with Javascript/script.js
@@ -572,3 +572,22 @@ console.log(mike.__proto__.__proto__);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+//
+//
+// ========================================
+// CHALLENGE #3
+// ========================================
+//
+// 1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property)
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo'
+// 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1. Then, log a message like this: 'Tesla going at 140km/h, with a charge of 22%'
+// 4. Create an eelctric car object and experiment with calling 'accelerate', 'brake', and 'cahrgeBattery' (charge to 90%). Notice what happens when you 'accelerate'
+
+// DATA SET
+// 'Tesla' going at 120km/h, with a charge of 23%
