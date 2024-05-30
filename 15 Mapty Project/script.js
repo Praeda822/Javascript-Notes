407ce724b90819937be65479e5b92c76f212d015
Patrick Kelly <patdkelly5@gmail.com>
2024-05-30 18:38:55 +1000

    Use geolocation API to display (((current location)))

--- 15 Mapty Project/script.js
+++ 15 Mapty Project/script.js
@@ -10,3 +10,25 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// ========================================
+// Using the Geo-location API
+// ========================================
+//
+// This takes two callback functions as an argument:
+// 1. To be called on success, so whenever the browser is able to successfully get the coordinates of the current position of the user
+// I'll call this function with one argument: the position parameter
+// 2. Error callback function, so the one that I'll define when an error occurs
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com.au/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your current location, bruv');
+    }
+  );
