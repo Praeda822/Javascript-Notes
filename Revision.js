330a3a80de90e8b0fb2e526b637e7478d5f124e7
Patrick Kelly <patdkelly5@gmail.com>
2024-05-31 22:26:38 +1000

    Rough as guts

--- Revision.js
+++ Revision.js
@@ -336,3 +336,35 @@ pkNew.calcAge();
 //========================================
 //========================================
 //========================================
+
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+// The three functions are:
+
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
+
+function createCounter(init) {
+  let currentValue = init;
+
+  return {
+    increment: () => {
+      currentValue += 1;
+      return currentValue;
+    },
+    decrement: () => {
+      currentValue -= 1;
+      return currentValue;
+    },
+    reset: () => {
+      currentValue = init;
+      return currentValue;
+    },
+  };
+}
+
+const counter = createCounter(5);
+counter.increment(); // 6
+counter.reset(); // 5
+counter.decrement(); // 4
