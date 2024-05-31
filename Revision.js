75e1b910c70669c2870dc5e5e060560e9f7a018c
Patrick Kelly <patdkelly5@gmail.com>
2024-05-31 22:32:05 +1000

    Second question attempt...

--- Revision.js
+++ Revision.js
@@ -359,3 +359,11 @@ const counter = createCounter(5);
 counter.increment(); // 6
 counter.reset(); // 5
 counter.decrement(); // 4
+
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+// You may assume the array is the output of JSON.parse.
+
+Array.prototype.last = function () {
+  return this.length > 0 ? this[this.length - 1] : -1;
+};
