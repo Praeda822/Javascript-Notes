22e1767a3bf405dd65abe869d4171be02125347d
Patrick Kelly <patdkelly5@gmail.com>
2024-06-01 17:57:31 +1000

    Dynamic input field logic

--- 15 Mapty Project/script.js
+++ 15 Mapty Project/script.js
@@ -77,3 +77,8 @@ form.addEventListener('submit', function (e) {
     .setPopupContent('Workout, bruvva')
     .openPopup();
 });
+
+inputType.addEventListener('change', function (e) {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
