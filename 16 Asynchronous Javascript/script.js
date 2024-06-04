73f08bbad5756fff9ef630328f3f421907aa3eae
Patrick Kelly <patdkelly5@gmail.com>
2024-06-04 16:11:34 +1000

    Asynchronous vs Synchronous code notes v2

--- 16 Asynchronous Javascript/script.js
+++ 16 Asynchronous Javascript/script.js
@@ -19,10 +19,23 @@ const countriesContainer = document.querySelector('.countries');
 // Asynchronous code doesn't wait for an synchronous task to finish its work
 // Callback functions on their own DO NOT make code asynchronous!!
 // So, asynchronous code is typically found in input/output operations, network requests, and/or fetching data - anything that could take an indiscriminate amount of time
+// Most of my APIs will be using a data format known as JSON, and JSON is basically just a javascript object that has been converted to a string
 //
 // AJAX
 // ========================================
 //
+// AJAX stands for Aynschronous Javascript and XML
+// AJAX allows us to communicate with remote web servers in an asynchronous way
+// Using AJAX calls I can request data from web servers in a dynamic way
+// I'd typically use an AJAX call to make a HTTP request to a web server to interact with that web server's API
+// This is known as an "Online" API, and an Online API can be defined as an application that is running on a web server that RECEIVES the requests for data, retrieves that data from somewhere inside a dodgy MySQL database, retrieiving it, and consequently sending back that retrieved data as its response
+// I'll be able to, one day, build my OWN web APIs (using something like Node.js) or by the use of 3rd-Party APIs
 //
 //
+// APIs
+// ========================================
 //
+// API stands for Application Programming Interface
+// An API is a piece of software that can be used by another software in order to allow applications to talk to one another
+// So, an API is essentially a self-contained piece of software that allow other pieces of software to interact with them
+// remember making the public methods in our ES6 class lecture, and then I was able to essentially "interact" with my objects I assigned with the methods
