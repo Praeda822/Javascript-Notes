a8ff3539280685ef721d846ff7d85d7bd02c451e
Patrick Kelly <patdkelly5@gmail.com>
2024-06-04 15:56:45 +1000

    Asynchronous vs Synchronous code notes v1

--- 16 Asynchronous Javascript/script.js
+++ 16 Asynchronous Javascript/script.js
@@ -4,3 +4,25 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// ========================================
+// Promises, Async/Await, AJAX and APIs
+// ========================================
+//
+// Asynchronous vs Synchronous Code
+// ========================================
+//
+// Most of the code I've been writing is known as Synchronous code
+// Synchronous code just means that my code is executed line-by-line by the exact order of execution, with each line of code waiting for the previous line to finish, with long-running operations block further code execution
+
+// Asynchronous code is executed AFTER a task that runs in the "background" finishes
+// Asynchronous code is NON-BLOCKING, which means the code can execute whilst something else is running in the executon context
+// Asynchronous code doesn't wait for an synchronous task to finish its work
+// Callback functions on their own DO NOT make code asynchronous!!
+// So, asynchronous code is typically found in input/output operations, network requests, and/or fetching data - anything that could take an indiscriminate amount of time
+//
+// AJAX
+// ========================================
+//
+//
+//
+//
