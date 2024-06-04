6eee01b1a43825b8abad2fa082947e686d60b23c
Patrick Kelly <patdkelly5@gmail.com>
2024-06-04 17:38:20 +1000

    Yeah nah this section is kinda fucked man

--- 16 Asynchronous Javascript/script.js
+++ 16 Asynchronous Javascript/script.js
@@ -71,12 +71,12 @@ const getCountryData = function (country) {
     // Remember I can use + to convert to a number
     const html = `
     <article class="country">
-    <img class="country__img" src="${Object.entries(data.flags)[0][1]}" />
+    <img class="country__img" src="${data.flag}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
+        +data.population / 26000000
       ).toFixed(1)} M people</p>
       <p class="country__row"><span>🗣️</span>${
         Object.entries(data.languages)[0][1]
