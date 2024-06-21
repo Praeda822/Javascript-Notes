30cbf7cf790670fdfe3101925ad4564487186bf0
Patrick Kelly <patdkelly5@gmail.com>
2024-06-21 15:15:22 +1000

    Added functionality to dynamically display recipe v1

--- 18 Forkify App/src/js/controller.js
+++ 18 Forkify App/src/js/controller.js
@@ -33,11 +33,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
     // 1. Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     // Convert promise result to JSON
     const data = await res.json();
@@ -161,4 +164,6 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+
+window.addEventListener('hashchange', showRecipe);
+window.addEventListener('load', showRecipe);
