15fcceea9151e5baabe962482573e3834b4b7dd0
Patrick Kelly <patdkelly5@gmail.com>
2024-06-21 14:42:16 +1000

    I GOT THE ICONS BACK HNNGGGG

--- 18 Forkify App/src/js/controller.js
+++ 18 Forkify App/src/js/controller.js
@@ -72,12 +72,12 @@ const showRecipe = async function () {
         <div class="recipe__info-buttons">
           <button class="btn--tiny btn--increase-servings">
             <svg>
-              <use href="src/img/icons.svg#icon-minus-circle"></use>
+              <use href="${icons}#icon-minus-circle"></use>
             </svg>
           </button>
           <button class="btn--tiny btn--increase-servings">
             <svg>
-              <use href="src/img/icons.svg#icon-plus-circle"></use>
+              <use href="${icons}#icon-plus-circle"></use>
             </svg>
           </button>
         </div>
@@ -85,12 +85,12 @@ const showRecipe = async function () {
 
       <div class="recipe__user-generated">
         <svg>
-          <use href="src/img/icons.svg#icon-user"></use>
+          <use href="${icons}#icon-user"></use>
         </svg>
       </div>
       <button class="btn--round">
         <svg class="">
-          <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+          <use href="${icons}#icon-bookmark-fill"></use>
         </svg>
       </button>
     </div>
@@ -103,7 +103,7 @@ const showRecipe = async function () {
             return ` 
             <li class="recipe__ingredient">
             <svg class="recipe__icon">
-              <use href="src/img/icons.svg#icon-check"></use>
+              <use href="${icons}#icon-check"></use>
             </svg>
             <div class="recipe__quantity">${ing.quantity}</div>
             <div class="recipe__description">
@@ -131,7 +131,7 @@ const showRecipe = async function () {
       >
         <span>Directions</span>
         <svg class="search__icon">
-          <use href="src/img/icons.svg#icon-arrow-right"></use>
+          <use href="${icons}#icon-arrow-right"></use>
         </svg>
       </a>
     </div>
