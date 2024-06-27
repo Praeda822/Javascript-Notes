0cefb8d6f217f8a637fecc6a4d2880f621d4b55c
Patrick Kelly <patdkelly5@gmail.com>
2024-06-27 16:35:15 +1000

    Bookmarking implementation | refactor #1

--- 18 Forkify App/src/js/controller.js
+++ 18 Forkify App/src/js/controller.js
@@ -26,6 +26,7 @@ const controlRecipes = async function () {
 
     // Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1. Loading recipe
     // Will return a promise - HANDLE IT
