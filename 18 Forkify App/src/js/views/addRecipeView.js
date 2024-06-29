fa67b136bbf268b2d36a20c9b544cf8a2fe3b26c
Patrick Kelly <patdkelly5@gmail.com>
2024-06-29 17:53:44 +1000

    Code..works, but i can't import addrecipeview to my controller?

--- 18 Forkify App/src/js/views/addRecipeView.js
+++ 18 Forkify App/src/js/views/addRecipeView.js
@@ -12,6 +12,7 @@ class AddRecipeView extends View {
   constructor() {
     super();
     this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
   }
 
   toggleWindow() {
@@ -20,7 +21,13 @@ class AddRecipeView extends View {
   }
 
   _addHandlerShowWindow() {
+    console.log(this._btnOpen);
     this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
   }
 
   _generateMarkup() {}
